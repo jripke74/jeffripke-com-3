1910c7631be1ebb6ec1b279d7e3797e358ccce57
Jeff Ripke <jripke74@gmail.com>
2022-01-19 15:47:39 -0800

    update styles

--- routes/base.routes.js
+++ routes/base.routes.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 
+router.get("/", function(req, res) {
+  res.render("main");
+});
+
 router.get("/401", function(req, res) {
   res.status(401).render("shared/401");
 });
