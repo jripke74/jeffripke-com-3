4acfaf298af10922220f1195229fe9b77309a848
Jeff Ripke <jripke74@gmail.com>
2022-02-12 09:31:07 -0800

    add index.ejs, about.ejs, and default.js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+const defaultRoutes = require('./routes/default');
 
-app.use(express.static("public"));
-app.use("/products/assets", express.static('product-data'))
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static('public'));
+app.use('/', defaultRoutes);
 app.use(express.urlencoded({ extended: false }));
 
 app.listen(3000);
