04595555246ead669a711d3f3aaeb86b50512b4b
Jeff Ripke <jripke74@gmail.com>
2022-03-01 07:33:07 -0800

    add contact and about pages

--- routes/default.js
+++ routes/default.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function (req, res) {
-  res.render('index');
+  res.render('index', { title: 'Home'});
 });
 
 router.get('/projects', function (req, res) {
@@ -11,7 +11,11 @@ router.get('/projects', function (req, res) {
 });
 
 router.get('/about', function (req, res) {
-  res.render('about');
+  res.render('about', { title: 'About' });
+});
+
+router.get('/contact', function(req, res) {
+  res.render('contact', { title: 'Contact Info' })
 });
 
 module.exports = router;
