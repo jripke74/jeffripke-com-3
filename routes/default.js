9a32f25f10a1b3705b89ce2abd00e00ea8ffcc39
Jeff Ripke <jripke74@gmail.com>
2022-03-01 07:22:27 -0800

    add /projects route

--- routes/default.js
+++ routes/default.js
@@ -6,7 +6,11 @@ router.get('/', function (req, res) {
   res.render('index');
 });
 
-router.get('/about', function(req, res) {
+router.get('/projects', function (req, res) {
+  res.render('projects');
+});
+
+router.get('/about', function (req, res) {
   res.render('about');
 });
 
