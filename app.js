392e512e980c17cc74a0736be4df1053f44d8195
Jeff Ripke <jripke74@gmail.com>
2022-03-05 07:51:59 -0800

    orgaanize code in app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
-app.use(defaultRoutes);
 app.use(express.urlencoded({ extended: false }));
+app.use(defaultRoutes);
 
 app.listen(3500);
